6ba0b06e9721c920e33301740a6fef13ac7411b0
marquessam <samuelnmarques@gmail.com>
2025-02-04 23:41:20 +0800

    Create games.js

---
+++ src/config/games.js
@@ -0,0 +1,29 @@
+// File: src/config/games.js
+const monthlyGames = [
+    {
+        month: 1,
+        year: 2024,
+        monthlyGame: {
+            gameId: "319",
+            title: "Chrono Trigger"
+        },
+        shadowGame: {
+            gameId: "10024",
+            title: "Mario Tennis"
+        }
+    },
+    {
+        month: 2,
+        year: 2024,
+        monthlyGame: {
+            gameId: "355",
+            title: "The Legend of Zelda: A Link to the Past"
+        },
+        shadowGame: {
+            gameId: "274",
+            title: "UN Squadron"
+        }
+    }
+];
+
+module.exports = { monthlyGames };
