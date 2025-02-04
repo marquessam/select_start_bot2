6c7820bc244af41372f56d618e12e789186822ba
marquessam <samuelnmarques@gmail.com>
2025-02-04 22:59:49 +0800

    Update index.js

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,1 +1,19 @@
-place holder
+// File: src/commands/index.js
+const fs = require('node:fs');
+const path = require('node:path');
+
+const loadCommands = (client) => {
+    const commandsPath = path.join(__dirname);
+    const commandFiles = fs.readdirSync(commandsPath)
+        .filter(file => file.endsWith('.js') && file !== 'index.js');
+
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        }
+    }
+};
+
+module.exports = { loadCommands };
