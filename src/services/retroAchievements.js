7e265a2ac06fc51195a34333e5a18468b5e62da3
marquessam <samuelnmarques@gmail.com>
2025-02-04 22:16:03 +0800

    Create retroAchievements.js

---
+++ src/services/retroAchievements.js
@@ -0,0 +1,46 @@
+// src/services/retroAchievements.js
+const axios = require('axios');
+
+class RetroAchievementsAPI {
+  constructor(username, apiKey) {
+    this.username = username;
+    this.apiKey = apiKey;
+    this.baseUrl = 'https://retroachievements.org/API';
+  }
+
+  async getUserProgress(raUsername, gameId) {
+    try {
+      const response = await axios.get(`${this.baseUrl}/API_GetGameInfoAndUserProgress.php`, {
+        params: {
+          z: this.username,
+          y: this.apiKey,
+          u: raUsername,
+          g: gameId,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching user progress:', error);
+      throw error;
+    }
+  }
+
+  async getUserRecentAchievements(raUsername, count = 10) {
+    try {
+      const response = await axios.get(`${this.baseUrl}/API_GetUserRecentAchievements.php`, {
+        params: {
+          z: this.username,
+          y: this.apiKey,
+          u: raUsername,
+          c: count,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching recent achievements:', error);
+      throw error;
+    }
+  }
+}
+
+module.exports = RetroAchievementsAPI;
