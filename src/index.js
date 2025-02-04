bc9ab0248e9a8ecf6a4c9e1cf1bdae01dd44045e
marquessam <samuelnmarques@gmail.com>
2025-02-04 23:24:04 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildIntegrations
+
   ]
 });
 
