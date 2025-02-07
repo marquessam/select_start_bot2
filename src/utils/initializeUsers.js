4ad0a6252e7ff9c0ce4175115b7936750a9b863e
marquessam <samuelnmarques@gmail.com>
2025-02-07 11:05:34 +0800

    Update initializeUsers.js

--- src/utils/initializeUsers.js
+++ src/utils/initializeUsers.js
@@ -2,24 +2,25 @@
 const User = require('../models/User');
 const { cleanupDatabase } = require('./cleanupDatabase');
 
-// Test group only
 const validUsers = [
-    "royek",             // Make sure these match exactly the case 
-    "Marquessam",        // you want to use for announcements
-    "lowaims",
-    "xelxlolox",
-    "hyperlincs"
+    "SirVaelion", "jvmunduruca", "xelxlolox", "EmsiRG", "Dangeel",
+    "hyperlincs", "BlackZWolf", "punchdrunkpengin", "Shigarui", "RuySan",
+    "LucasTheBeard", "royek", "NiterZ7", "thardwardy", "nxsnexus",
+    "Newtim", "R3dEagle", "JRevo", "MuttonchopMac", "joebobdead",
+    "zckttck", "tragicnostalgic", "Magus508", "ShminalShmantasy", "lowaims",
+    "ParanoidPunky", "Audex", "Xsiverx", "Marquessam", "Dest404",
+    "wastelanderone", "Lyubphim", "DearYou"
 ];
 
 async function initializeUsers() {
     try {
-        console.log('Starting user initialization with test group...');
+        console.log('Starting user initialization...');
         
         // First, remove ALL existing users
         await User.deleteMany({});
         console.log('Cleared existing users');
         
-        // Create our test users
+        // Create new users
         const userPromises = validUsers.map(username => {
             return User.findOneAndUpdate(
                 { raUsername: { $regex: new RegExp(`^${username}$`, 'i') } },
@@ -34,7 +35,7 @@ async function initializeUsers() {
         await Promise.all(userPromises);
         
         const totalUsers = await User.countDocuments();
-        console.log(`Initialized ${totalUsers} test users successfully`);
+        console.log(`Initialized ${totalUsers} users successfully`);
     } catch (error) {
         console.error('Error initializing users:', error);
         throw error;
