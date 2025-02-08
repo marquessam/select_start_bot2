8ce6afbb1bb569e363a63f06e232db9672b0ffef
marquessam <samuelnmarques@gmail.com>
2025-02-08 20:59:17 +0800

    Update Nomination.js

--- src/models/Nomination.js
+++ src/models/Nomination.js
@@ -4,15 +4,21 @@ const mongoose = require('mongoose');
 const nominationSchema = new mongoose.Schema({
   userId: {
     type: String,
-    required: true,
+    default: 'legacy', // For legacy nominations, userId is set to 'legacy'
   },
   gameTitle: {
     type: String,
     required: true,
   },
   gameId: {
-    type: Number,
-    required: true,
+    type: Number, // For legacy nominations you might not have a gameId, so you can consider it optional or assign 0.
+    default: 0,
+  },
+  platform: {
+    type: String,
+  },
+  nominatedBy: {
+    type: String, // The nominator's name (e.g., Legacy Import or a username)
   },
   voteMonth: {
     type: String,
