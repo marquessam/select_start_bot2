66c6789e350386dce15064e9e385d1051a633d55
marquessam <samuelnmarques@gmail.com>
2025-02-10 15:00:20 +0800

    Update AwardType.js

--- src/enums/AwardType.js
+++ src/enums/AwardType.js
@@ -1,9 +1,10 @@
 // File: src/enums/AwardType.js
+
 const AwardType = {
     NONE: 0,
     PARTICIPATION: 1,
-    BEATEN: 4,     // PARTICIPATION (1) + beaten bonus (3)
-    MASTERED: 7,   // PARTICIPATION (1) + beaten (3) + mastery (3)
+    BEATEN: 4,     // 1 (participation) + 3 (beaten)
+    MASTERED: 7,   // 1 (participation) + 3 (beaten) + 3 (mastery)
     MANUAL: 999    // Special type for manual awards
 };
 
