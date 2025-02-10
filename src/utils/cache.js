5b86c3045ec983267c808907f66ce67c01219dc5
marquessam <samuelnmarques@gmail.com>
2025-02-10 14:15:59 +0800

    Update cache.js

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -1,5 +1,4 @@
 // File: src/utils/cache.js
-
 class Cache {
     constructor(ttl = 300000) { // Default TTL: 5 minutes (300000ms)
         this.cache = new Map();
@@ -28,6 +27,27 @@ class Cache {
 
     clear() {
         this.cache.clear();
+    }
+
+    // Add size method
+    size() {
+        return this.cache.size;
+    }
+
+    // Add method to get all valid keys
+    keys() {
+        const now = Date.now();
+        return Array.from(this.cache.entries())
+            .filter(([_, data]) => now - data.timestamp <= this.ttl)
+            .map(([key]) => key);
+    }
+
+    // Add method to get all valid entries
+    entries() {
+        const now = Date.now();
+        return Array.from(this.cache.entries())
+            .filter(([_, data]) => now - data.timestamp <= this.ttl)
+            .map(([key, data]) => [key, data.value]);
     }
 }
 
