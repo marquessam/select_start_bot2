cf67cd5dfbf49a5a926c4d36163c6ef1c0157fb1
marquessam <samuelnmarques@gmail.com>
2025-03-31 17:33:12 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { fileURLToPath } from 'url';
 import cron from 'node-cron';
 import statsUpdateService from './services/statsUpdateService.js';
 import achievementFeedService from './services/achievementFeedService.js';
-import enhancedAchievementFeedService from './services/enhancedAchievementFeedService.js';
 import monthlyTasksService from './services/monthlyTasksService.js';
 import arcadeService from './services/arcadeService.js';
 
@@ -96,7 +95,6 @@ client.once(Events.ClientReady, async () => {
 
         // Set client for services
         achievementFeedService.setClient(client);
-        enhancedAchievementFeedService.setClient(client);
         monthlyTasksService.setClient(client);
         arcadeService.setClient(client);
 
@@ -113,14 +111,6 @@ client.once(Events.ClientReady, async () => {
             console.log('Running achievement feed check...');
             achievementFeedService.start().catch(error => {
                 console.error('Error in achievement feed check:', error);
-            });
-        });
-        
-        // Schedule enhanced achievement feed checks every 15 minutes
-        cron.schedule('*/15 * * * *', () => {
-            console.log('Running enhanced achievement feed check...');
-            enhancedAchievementFeedService.start().catch(error => {
-                console.error('Error in enhanced achievement feed check:', error);
             });
         });
 
@@ -153,9 +143,6 @@ client.once(Events.ClientReady, async () => {
         
         // Run initial achievement feed check
         await achievementFeedService.start();
-        
-        // Run initial enhanced achievement feed check
-        await enhancedAchievementFeedService.start();
         
         // Run initial arcade service check
         await arcadeService.start();
