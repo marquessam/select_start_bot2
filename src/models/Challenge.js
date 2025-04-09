2c103667301dacebbd9d67028195a0cbc0026bda
marquessam <samuelnmarques@gmail.com>
2025-04-09 12:52:05 +0800

    Update Challenge.js

--- src/models/Challenge.js
+++ src/models/Challenge.js
@@ -56,6 +56,25 @@ const challengeSchema = new mongoose.Schema({
         type: Boolean,
         required: true
     },
+    // New fields for storing game metadata
+    monthly_game_title: {
+        type: String
+    },
+    monthly_game_icon_url: {
+        type: String
+    },
+    monthly_game_console: {
+        type: String
+    },
+    shadow_game_title: {
+        type: String
+    },
+    shadow_game_icon_url: {
+        type: String
+    },
+    shadow_game_console: {
+        type: String
+    }
 });
 
 // Add method to check if shadow game is revealed
@@ -69,6 +88,37 @@ challengeSchema.methods.isCurrentGame = function() {
     const challengeMonth = this.date.getMonth();
     const challengeYear = this.date.getFullYear();
     return challengeMonth === now.getMonth() && challengeYear === now.getFullYear();
+};
+
+// New method to update game metadata
+challengeSchema.methods.updateGameMetadata = async function(retroAPI) {
+    try {
+        // Update monthly game metadata
+        if (this.monthly_challange_gameid) {
+            const gameInfo = await retroAPI.getGameInfo(this.monthly_challange_gameid);
+            if (gameInfo) {
+                this.monthly_game_title = gameInfo.title;
+                this.monthly_game_icon_url = gameInfo.imageIcon;
+                this.monthly_game_console = gameInfo.consoleName;
+            }
+        }
+
+        // Update shadow game metadata if revealed
+        if (this.shadow_challange_gameid && this.shadow_challange_revealed) {
+            const shadowGameInfo = await retroAPI.getGameInfo(this.shadow_challange_gameid);
+            if (shadowGameInfo) {
+                this.shadow_game_title = shadowGameInfo.title;
+                this.shadow_game_icon_url = shadowGameInfo.imageIcon;
+                this.shadow_game_console = shadowGameInfo.consoleName;
+            }
+        }
+        
+        await this.save();
+        return true;
+    } catch (error) {
+        console.error('Error updating game metadata:', error);
+        return false;
+    }
 };
 
 // The date field is already indexed due to unique: true
